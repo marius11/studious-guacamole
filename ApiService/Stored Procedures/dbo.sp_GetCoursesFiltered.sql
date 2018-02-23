c381411fcb55b9d2ae38714a3358d74a97ec66a8
Marius Moldovan <mariusmnd@hotmail.com>
2018-02-23 13:44:33 +0200

    added stored procedures for filtering students and courses

---
+++ ApiService/Stored Procedures/dbo.sp_GetCoursesFiltered.sql
@@ -0,0 +1,11 @@
+﻿CREATE PROCEDURE [dbo].[sp_GetCoursesFiltered]
+	@search_query NVARCHAR(64),
+	@per_page INT
+AS
+BEGIN
+	SET NOCOUNT ON;
+
+	SELECT TOP (@per_page) Id, Name
+	FROM Courses
+	WHERE Name LIKE '%' + @search_query + '%'
+END
---
+++ ApiService/Stored Procedures/dbo.sp_GetStudentsFiltered.sql
@@ -0,0 +1,11 @@
+﻿CREATE PROCEDURE [dbo].[sp_GetStudentsFiltered]
+	@search_query NVARCHAR(64),
+	@per_page INT
+AS
+BEGIN
+	SET NOCOUNT ON;
+
+	SELECT TOP (@per_page) Id, FirstName, LastName
+	FROM Students
+	WHERE FirstName LIKE '%' + @search_query + '%' OR LastName LIKE '$' + @search_query + '$'
+END
