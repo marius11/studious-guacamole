5a9293741b623a258fc5cfb36834dc7b774237e6
Marius Moldovan <mariusmnd@hotmail.com>
2018-03-01 10:44:48 +0200

    added stored procedures to update students and courses

---
+++ ApiService/Stored Procedures/dbo.UpdateCourse.sql
@@ -0,0 +1,11 @@
+﻿CREATE PROCEDURE [dbo].[UpdateCourse]
+	@Id INT,
+	@Name NVARCHAR(128)
+AS
+BEGIN
+	SET NOCOUNT ON;
+
+	UPDATE Courses
+	SET Name = @Name
+	WHERE Id = @Id
+END
---
+++ ApiService/Stored Procedures/dbo.UpdateStudent.sql
@@ -0,0 +1,16 @@
+﻿CREATE PROCEDURE [dbo].[UpdateStudent]
+	@Id INT,
+	@FirstName NVARCHAR(64) = NULL,
+	@LastName NVARCHAR(64) = NULL
+AS
+
+IF COALESCE(@FirstName, @LastName) IS NOT NULL
+BEGIN
+	SET NOCOUNT ON;
+
+	UPDATE Students
+	SET 
+		FirstName = COALESCE(@FirstName, FirstName),
+		LastName = COALESCE(@LastName, LastName)
+	WHERE Id = @Id
+END
