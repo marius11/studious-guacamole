7c608a9721c0b8d56ef2223938991194445e442e
Marius Moldovan <mariusmnd@hotmail.com>
2018-03-06 14:42:46 +0200

    added a stored procedure to delete a course from a student

---
+++ ApiService/Stored Procedures/dbo.DeleteCourseFromStudent.sql
@@ -0,0 +1,10 @@
+﻿CREATE PROCEDURE [dbo].[DeleteCourseFromStudent]
+	@CourseId INT,
+	@StudentId INT
+AS
+BEGIN
+	SET NOCOUNT ON;
+
+	DELETE FROM StudentCourses
+	WHERE Course_Id = @CourseId AND Student_Id = @StudentId
+END
